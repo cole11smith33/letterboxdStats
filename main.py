b9e1a2a66d56a16aecebfb33386c95a448bc8d02
cole11smith33 <cole11smith33@gmail.com>
2025-06-09 16:54:02 -0400

    fixed issue where tools.py was not imported and used properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # using the letterboxdpy as a temporary way to webscrap user information
 from letterboxdpy import movie
 from letterboxdpy import list
-import tools.py
+from tools import *
+
+setUser()
