42155f747932d3e6e226af152aa34dfc28ea760f
cole11smith33 <cole11smith33@gmail.com>
2025-06-09 21:48:43 -0400

    added ability to get user genre information to tools.py file

--- tools.py
+++ tools.py
@@ -1,6 +1,14 @@
 from letterboxdpy import user
 
 def setUser():
-    username = input(str("Please enter the username of the letterboxd account you wish to see the statistics for: "))
-    username = user.User(username)
-    print(username)
+    name = input(str("Please enter the username of the letterboxd account you wish to see the statistics for: "))
+    username = user.User(name) # stored as type letterboxdpy.user.User
+    #print(type(username))
+    #print(username.stats)
+    #print(username.watchlist_length)
+
+def getUserGenreInfo(username):
+    try:
+        print(user.user_genre_info(username))
+    except:
+        print("Not enough user information to view genre distribution")
