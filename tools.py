7ccbec5a4021685c74bf1f244692326962eaf014
cole11smith33 <cole11smith33@gmail.com>
2025-06-09 22:28:47 -0400

    cleaned up getUserWatchlistLength function

--- tools.py
+++ tools.py
@@ -3,9 +3,10 @@ from letterboxdpy import user
 def setUser():
     name = input(str("Please enter the username of the letterboxd account you wish to see the statistics for: "))
     username = user.User(name) # stored as type letterboxdpy.user.User
-    #print(type(username))
-    #print(username.stats)
-    #print(username.watchlist_length)
+    return username # as a User object
+
+def getUserWatchlistLength(username):
+    return username.watchlist_length
 
 def getUserGenreInfo(username):
     try:
